511606798ca23ece95aa25883993a01a5cd91f06
gaetan <gaetan.bloch@gmail.com>
2020-03-12 10:57:06 +0100

    Inject dependencies using qualifiers to differentiate implementations of the GreetingService

--- src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,6 +1,7 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
@@ -8,7 +9,7 @@ public class ConstructorInjectedController implements InjectedController {
 
     private final GreetingService greetingService;
 
-    public ConstructorInjectedController(GreetingService greetingService) {
+    public ConstructorInjectedController(@Qualifier("constructorGettingService") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
